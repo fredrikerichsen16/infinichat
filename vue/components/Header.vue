32135e4f5b4a0b3fe0fd58647ef5ad9a8a3d1c22
Duculet <ducu@programmer.net>
2020-04-24 15:04:00 +0300

    Test

---
+++ vue/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+        <a>Home</a>
+        <a>Posts</a>
+        <a>Contact</a>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
