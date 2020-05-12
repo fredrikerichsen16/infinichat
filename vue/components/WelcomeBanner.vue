66ee0680eb34ee24b56724bc92c741791e736ffb
rossmedcalf <58558938+rossmedcalf@users.noreply.github.com>
2020-05-12 17:40:19 +0200

    welcome banner unfinished

---
+++ vue/components/WelcomeBanner.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header>{{title}}</header>
+        <div>{{content}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "WelcomeBanner",
+        data() {
+            return {
+                title: 'Welcome to InfiniChat!',
+                content: 'please login or signup below'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
